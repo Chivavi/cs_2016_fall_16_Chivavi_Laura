7365a50aa33a4e8b8db1328639717d882c8bfa4b
Chivavi <lauchivavi@hotmail.com>
2017-06-16 22:09:56 +0300

    Add files via upload

---
+++ corrections/9.1 correction.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+       if (n == 0){
+        return 0;
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else {
+        return fib(n-1)+fib(n-2);
+    }
+}
+
+int main()
+{
+    int n = 0;
+    int i = 0;
+    printf("Enter N\n");
+    scanf("%i", &n);
+    for (i=0;i<=n;++i){
+        printf("%i|%i\n", i, fib(i));
+    }
+    return 0;
+}
+
+
